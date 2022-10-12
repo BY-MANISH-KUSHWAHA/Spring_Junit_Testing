2dd6a06622916e5a7a75c0a440646467e7e91f3a
BY-MANISH-KUSHWAHA <manish.kushwaha@blueyonder.com>
2022-10-12 10:09:32 +0530

    XML based Bean Configuration and Test using app-context.xml inside resources

--- src/main/java/org/example/movie/ContentFiltering.java
+++ src/main/java/org/example/movie/ContentFiltering.java
@@ -1,5 +1,6 @@
 package org.example.movie;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
--- src/test/java/Junit/XmlConfigMovieRecommendedTest.java
+++ src/test/java/Junit/XmlConfigMovieRecommendedTest.java
@@ -1,5 +1,37 @@
 package Junit;
 
-public class XmlConfigMovieRecommendedTest {
 
+import org.example.movie.MovieRecommenderSystem;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "/app-context.xml")
+public class XmlConfigMovieRecommendedTest {
+    //@Qualifier("MovieRecommenderSystem")
+    @Autowired
+    MovieRecommenderSystem recommenderSystem;
+
+    @Test
+    public void TestRecommendedMovie(){
+        List<String> movies = new ArrayList<>();
+        movies.add("Iron Man");
+        movies.add("Iron Man 2");
+        movies.add("Iron Man 3");
+
+        List<String> resultMovies = recommenderSystem.RecommendedMovieByFilter();
+        System.out.println(resultMovies);
+        assertArrayEquals(new List[]{movies},new List[]{resultMovies});
+
+    }
 }
